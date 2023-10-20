dfd0f19860951db6add7891f8470eebacee98a62
DimitriiTrater <dima1603040@gmail.com>
2023-10-21 00:09:15 +0200

    changes in dirs

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 /usr/bin/cmake --no-warn-unused-cli -S./ -B./build -G Ninja
 
-/usr/bin/cmake --build ./build --config Debug --target all --
+/usr/bin/cmake --build /build --config Debug --target linux --
 
-./sorter_app
+/build/code/sorter_app/linux/linux
 
